2d363f9b2c50ce44e11233500179e20edaa0b8d3
k生活 <vince.chen@kingold.com>
2023-05-29 18:32:31 +0800

    Update for KG

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
-export const OWNER = "linjungz";
-export const REPO = "azure-chatgpt-ui";
+export const OWNER = "";
+export const REPO = "";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 // export const UPDATE_URL = `${REPO_URL}#%E4%BF%9D%E6%8C%81%E6%9B%B4%E6%96%B0-keep-updated`;
 // export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
